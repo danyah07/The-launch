e2858e17aee8575a576ad743d0457038b7101fb0
danyah07 <danyahal660@gmail.com>
2025-09-29 19:12:24 +0300

    Initial Commit

---
+++ The launch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  The launch
+//
+//  Created by Danyah ALbarqawi on 29/09/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ The launch/The_launchApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_launchApp.swift
+//  The launch
+//
+//  Created by Danyah ALbarqawi on 29/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct The_launchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
