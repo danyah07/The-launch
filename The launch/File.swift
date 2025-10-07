4acf4e8c2152a46cd4bed90b10276ea06ccd505a
danyah07 <danyahal660@gmail.com>
2025-10-07 12:42:31 +0300

    njnn

---
+++ The launch/File.swift
@@ -0,0 +1,13 @@
+HistoryView(
+                                answers: [
+                                    "How do you feel about yourself now vs. day one?": "I feel more productive.",
+                                    "If you had to restart your streak, what would you change?": "I would not miss any days.",
+                                    "Write a note to your future self": "Keep going forward!"
+                                ],
+                                completedHabit: Habit(
+                                    name: "Reading",
+                                    emoji: "📚",
+                                    progress: 30,
+                                    goal: 30
+                                )
+                            )
